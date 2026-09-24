919481436870ec6e56381bfe5d8ad0e76c863078
Yukina_V <mcbbs1145@outlook.com>
2026-09-24 11:17:00 +0800

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-# F:\Project\Project\run.py
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from ai_chat.__main__ import main   # 按你实际的函数名改
-
-if __name__ == "__main__":
-    main()
